5316b6c8d8c4a562448ca4b05ec0e4c29ae47640
Manutsawin <manassawin369@gmail.com>
2022-11-23 13:16:47 +0700

    sad

--- src/httpConfig.ts
+++ src/httpConfig.ts
@@ -1,4 +1,4 @@
-export const PAYMENT_SERVICE_URL = 'https://7695-110-164-126-99.ap.ngrok.io/'
+export const PAYMENT_SERVICE_URL = 'https://9018-223-24-186-108.ap.ngrok.io/'
 
 export const TRANSACTION_SERVICE_URL = 'https://6739-2001-44c8-4082-bcdc-5131-9b10-6f9-ba99.ap.ngrok.io/'
 
